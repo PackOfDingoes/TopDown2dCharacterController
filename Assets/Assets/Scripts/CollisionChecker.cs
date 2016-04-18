ed73c27f5e8ecaec649f4854c0f21616cbe9c1f4
PackOfDingoes <silverback.jed@gmail.com>
2016-04-18 22:00:30 +1000

    Added collision detection and cleaned code
    made it so a collider moves first, detecting if there

---
+++ Assets/Assets/Scripts/CollisionChecker.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CollisionChecker : MonoBehaviour 
+{
+	public bool isColliding = false;
+	public string ObstacleTag = "Obstacle";
+
+	void Start () 
+	{
+	
+	}
+
+	void Update () 
+	{
+	
+	}
+
+	void OnTriggerEnter2D(Collider2D other)
+	{
+		if(other.gameObject.tag == ObstacleTag)
+		{
+			isColliding = true;
+		}
+	}
+}
--- Assets/Assets/Scripts/TopDown2DCharacterController.cs
+++ Assets/Assets/Scripts/TopDown2DCharacterController.cs
@@ -10,24 +10,30 @@ public class TopDown2DCharacterController : MonoBehaviour
 	private Vector2 oldPos;
 	private Vector2 currentPos;
 	private Vector2 nextPos;
+	public GameObject collisionCheckerObject;
+	private CollisionChecker collisionChecker;
 	private float moveTime;
+	public string ObstacleTag = "Obstacle";
+	public float gridSize = 1.0f;
 	public float speed = 2f;
 	private bool isMoving = false;
+	private bool canMove = true;
 
 	void Awake ()
     {
 //		rb2d = GetComponent<Rigidbody2D>();
+		collisionChecker = collisionCheckerObject.GetComponent<CollisionChecker>();
 		currentPos = this.transform.position;
 	}
 
 	void Update ()
     {
-		Move();
+		
 	}
 
     void FixedUpdate ()
     {
-		
+		Move();
     }
 
 	void Move ()
@@ -37,33 +43,37 @@ public class TopDown2DCharacterController : MonoBehaviour
 		{
 			if (Input.GetButton("Right"))
 			{
-				oldPos = this.transform.position;
-				nextPos = new Vector2(currentPos.x + 1.0f, currentPos.y);
-				moveTime = 0f;
+				MoveDirection("right");
 			}
 			if (Input.GetButton("Left"))
 			{
-				oldPos = this.transform.position;
-				nextPos = new Vector2(currentPos.x - 1.0f, currentPos.y);
-				moveTime = 0f;
+				MoveDirection("left");
 			}
 			if (Input.GetButton("Up"))
 			{
-				oldPos = this.transform.position;
-				nextPos = new Vector2(currentPos.x, currentPos.y + 1.0f);
-				moveTime = 0f;
+				MoveDirection("up");
 			}
 			if (Input.GetButton("Down"))
 			{
-				oldPos = this.transform.position;
-				nextPos = new Vector2(currentPos.x, currentPos.y - 1.0f);
-				moveTime = 0f;
+				MoveDirection("down");
 			}
 		}
+			
+		if(collisionChecker.isColliding == false)
+		{
+			currentPos = nextPos;
 
-		currentPos = nextPos;
+			this.transform.position = Vector2.Lerp(oldPos, nextPos, moveTime);
+		}
 
-		this.transform.position = Vector2.Lerp(oldPos, nextPos, moveTime);
+		if(collisionChecker.isColliding == true)
+		{
+			nextPos = oldPos;
+			currentPos = oldPos;
+			collisionChecker.isColliding = false;
+		}
+
+		collisionCheckerObject.transform.position = currentPos;
 
 		if(this.transform.position.x == currentPos.x && this.transform.position.y == currentPos.y)
 		{
@@ -74,5 +84,30 @@ public class TopDown2DCharacterController : MonoBehaviour
 		{
 			isMoving = true;
 		}
+	}
+
+	void MoveDirection(string direction)
+	{
+		oldPos = this.transform.position;
+		switch (direction)
+		{
+			case "right":
+				nextPos = new Vector2(currentPos.x + gridSize, currentPos.y);
+				break;
+			case "left":
+				nextPos = new Vector2(currentPos.x - gridSize, currentPos.y);
+				break;
+			case "up":
+				nextPos = new Vector2(currentPos.x, currentPos.y + gridSize);
+				break;
+			case "down":
+				nextPos = new Vector2(currentPos.x, currentPos.y - gridSize);
+				break;
+			default:
+				nextPos = currentPos;
+				break;
+		}
+		collisionCheckerObject.transform.position = nextPos;
+		moveTime = 0f;
 	}
 }
